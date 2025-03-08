a023fd8f2ce225ee02530ed1500e1a8081f0f543
Sandeep <sandeepdewangan7@gmail.com>
2025-03-08 22:24:20 +0530

    Persisting state value persist the state value to localStorage

---
+++ src/App2.jsx
@@ -0,0 +1,18 @@
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+const username = "sandeep";
+const currentUser = atomWithStorage("username", username);
+
+function App2() {
+  const [user, setUser] = useAtom(currentUser);
+
+  return (
+    <>
+      <label>Current User is: {user}</label>
+      <input type="text" onChange={(e) => setUser(e.target.value)} />
+    </>
+  );
+}
+
+export default App2;
